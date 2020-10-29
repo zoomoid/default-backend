cf68064425817f4c68471e049c52725bd6bea100
Alexander Bartolomey <occloxium@gmail.com>
2020-10-29 01:20:52 +0100

    build: :rocket: docker image update
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,20 @@
-FROM node:latest
+FROM node:latest AS builder
+
+WORKDIR /app
+
+COPY package.json .
+COPY yarn.lock .
+
+RUN yarn
+
+COPY . .
+
+RUN yarn run build
+
+FROM nginx:alpine
+
+COPY nginx.conf /etc/nginx/conf.d/default.conf
+
+COPY --from=builder /app/dist /var/www/html
+
+RUN nginx -t
--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="container mx-auto flex flex-col justify-center h-screen" id="app">
-    <img class="w-full px-4" src="./assets/undraw_Taken_re_yn20.svg" />
-    <h1 class="xl:text-6xl">This route left our realms! <span class="xl:text-2xl">Make sure you requested a valid service URL.</span></h1>
-    <ul>
-      <a class="xl:text-2xl" href="https://alexander.bartolomey.me">https://alexander.bartolomey.me</a>
-      <a class="xl:text-2xl" href="https://zoomoid.de">https://zoomoid.de</a>
-    </ul>
+  <div class="mx-auto flex flex-col justify-center h-screen bg-black" id="app">
+    <div class="max-w-1/2 mx-auto">
+      <img class="w-full block mx-auto px-4" src="./assets/undraw_Taken_re_yn20.svg" />
+      <h1 class="xl:text-6xl">This route left our realms! <span class="xl:text-2xl">Make sure you requested a valid URL.</span></h1>
+      <ul>
+        <a class="xl:text-2xl" href="https://alexander.bartolomey.me">https://alexander.bartolomey.me</a>
+        <a class="xl:text-2xl" href="https://zoomoid.de">https://zoomoid.de</a>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -20,6 +22,7 @@ export default {
 #app
   -webkit-font-smoothing: antialiased
   -moz-osx-font-smoothing: grayscale
+  @apply text-white
   h1
     @apply text-4xl px-4 font-semibold leading-tight
     span
